3933b1cb500cd9c6c2f26d672164ae946336c092
longkdph32726 <khuatleu1610@gmail.com>
2024-10-29 16:02:40 +0700

    tạo quan hệ comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    public function User (){
+        return $this->belongsTo(User::class);
+    }
+    public  function Product(){
+        return $this->belongsTo(Product::class);
+    }
 }
--- database/migrations/2024_10_02_180208_create_comments_table.php
+++ database/migrations/2024_10_02_180208_create_comments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->integer('parent_id');
+            $table->integer('parent_id')->default(0);
             $table->integer('product_id');
             $table->integer('user_id');
             $table->longText('content');
